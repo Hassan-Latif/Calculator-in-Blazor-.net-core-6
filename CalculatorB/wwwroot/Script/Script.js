fc77ce7aeea0e6a1ac70c1ff69581ab608573974
Hassan Latif <ranashb520@gmail.com>
2021-12-16 17:10:14 +0500

    Changes are commited

---
+++ CalculatorB/wwwroot/Script/Script.js
@@ -0,0 +1,40 @@
+﻿let validResult = 0
+const idOfResultInput = "result"
+const errorMessage = "Syntax Error"
+const format = /[!@#$%^&()_\\[\]{};':"|,.<>\?]+/
+
+function clearScreen() {
+    document.getElementById(idOfResultInput).value = "";
+}
+
+function displayOnScreen(val) {
+    if (validResult === 1) {
+        clearScreen();
+        validResult = 0
+    }
+    document.getElementById(idOfResultInput).value += val
+}
+
+function calculate() {
+    try {
+        let answer = eval(document.getElementById(idOfResultInput).value)
+        let currentValue = document.getElementById(idOfResultInput).value
+
+        if (currentValue !== '') {
+            if (answer === undefined || format.test(currentValue)) {
+                document.getElementById(idOfResultInput).value = errorMessage
+                validResult = 1
+            }
+            else {
+                document.getElementById(idOfResultInput).value = answer
+                validResult = 1
+            }
+        }
+
+
+    }
+    catch (err) {
+        document.getElementById(idOfResultInput).value = errorMessage;
+        validResult = 1
+    }
+}
